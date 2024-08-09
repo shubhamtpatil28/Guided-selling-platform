7f5fc742f64234761276ae6ba01cb06c19d71742
shubhamtpatil28 <patilshubham2808@gmail.com>
2024-08-09 18:15:47 +0530

    bucket name change

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "mydev-project-terraform-batch-24"
+    bucket = "my-bucket-np"
     key = "main"
     region = "ap-south-1"
     dynamodb_table = "my-dynamodb-table"
